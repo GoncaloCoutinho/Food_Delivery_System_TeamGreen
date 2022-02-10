6a351d5fb7c6fb82ad2d76eaf66c969262f3f113
Ayush Goyal <ayushgoyal.2196@gmail.com>
2022-02-11 00:05:06 +0100

    Added relevant comments

--- ayush-use-case/subscribe.py
+++ ayush-use-case/subscribe.py
@@ -5,12 +5,12 @@ import json
 from twilio.rest import Client
 from multiprocessing import Process
 
-r = redis.Redis(charset="utf-8", decode_responses=True)
+r = redis.Redis(charset="utf-8", decode_responses=True) #establishing the redis connection
 
-
+# Subscribing to the channel on redis server
 def sub(name: str):
     pubsub = r.pubsub()
-    pubsub.subscribe("broadcast")
+    pubsub.subscribe("broadcast") #'broadcast' is the channel name
     for message in pubsub.listen():
         if message.get("type") == "message":
             data = json.loads(message.get("data"))
@@ -22,11 +22,12 @@ def sub(name: str):
             body = data.get("message")
             from_ = data.get("from")
             to = data.get("to")
-
+            
+            # Connection with Twilio for SMS sending
             client = Client(account_sid, auth_token)
             message = client.messages.create(from_=from_, to=to, body=body)
             print("message id: %s" % message.sid)
 
 
 if __name__ == "__main__":
-    Process(target=sub, args=("reader1",)).start()
+    Process(target=sub, args=("reader1",)).start() # Starts the subscribe method and listens on the server
